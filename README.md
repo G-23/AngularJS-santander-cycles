1abe0fe40b58ad6211b4b161a8ec07b144353594
Simone De Luca <simdeluca92@gmail.com>
2015-08-07 10:40:01 +0100

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# AngularJS-santander-cycles
-A simple app that lets you find Santander cycles docking stations around you.
+# AngularJS-weather-forecast
+==================================================
+
+A simple app which lets you find find Santander cycles docking stations around you by entering your address or postcode.
+
+The app makes a request to google maps api to geodecode the address entered, it then sends the latitude and longitude data to the tfl api which returns a list of Santander cycles stations docking within 2miles from the entered address.
+
+Try it live: http://simonedeluca.github.io/AngularJS-santander-cycles/app/
+
+## Requirements
+
+- [Node and npm](http://nodejs.org)
+
+## Installation
+
+1. Clone the repository: `git clone https://github.com/Simonedeluca/AngularJS-weather-forecast.git`
+2. Install the application: `npm install`
+3. Start the server: `npm start`
+4. View in browser at `http://localhost:8000`
---
+++ app/app.js
@@ -0,0 +1,82 @@
+(function(){
+	var cyclesApp = angular.module('cycles', []);
+
+	cyclesApp.service('GeoLocation', function($q, $timeout) {
+		var geocoder = new google.maps.Geocoder();
+
+		this.requestLocation = function(address) {
+			var deferred = $q.defer();
+
+			$timeout(function() {
+				geocoder.geocode( { 'address': address}, function(result, status) {
+					if (status == google.maps.GeocoderStatus.OK) {
+						deferred.resolve(result[0], status);
+					} else {
+						deferred.resolve(result[0], status);
+					}
+				});
+			}, 100);
+
+			return deferred.promise;
+		};
+	});
+
+	cyclesApp.service('Cycles', function(GeoLocation, $http, $q, $timeout) {
+		this.getBikes = function(data) {
+			var deferred = $q.defer();
+
+			$timeout(function() {
+				$http.get('https://api.tfl.gov.uk/BikePoint?lat=' + data.geometry.location.G + '&lon=' + data.geometry.location.K + '&radius=2000&app_id=53f9784a&app_key=acacc3e2c79c8bf49bf9315e6e8dec5f ')
+				.then(function(result) {
+					deferred.resolve(result);
+				});
+			}, 100);
+
+			return deferred.promise;
+		};
+	});
+
+	cyclesApp.controller('cyclesController', function($scope, GeoLocation, Cycles, $http) {
+		$scope.messages = {
+			invalidAddress: false,
+			noResults: false
+		};
+
+		$scope.findBikes = function() {
+			$scope.bikes = [];
+
+			GeoLocation.requestLocation($scope.address)
+			.then(function(data){
+				if(data == undefined) {
+					$scope.messages.invalidAddress = true;
+					$scope.messages.noResults = false;
+				} else {
+					Cycles.getBikes(data)
+					.then(function(result){
+						if(result.data.places.length > 0) {
+							$scope.fetchResults(result.data);
+							$scope.messages.noResults = false;
+						} else {
+							$scope.messages.noResults = true;
+						}
+					});
+					$scope.messages.invalidAddress = false;
+				}
+			}, function() {
+				alert('Sorry there was an error in your request, please try again in few minutes.');
+			});
+		};
+
+		$scope.fetchResults = function(data) {
+
+			angular.forEach(data.places, function(place) {
+				$scope.bikes.push( {
+					name: place.commonName,
+					distance: (Math.round(place.distance) *0.000621371192).toFixed(2),
+					availableBikes: place.additionalProperties[6].value,
+					emptyDocks: place.additionalProperties[7].value
+				});
+			});
+		};
+	});	
+})();
